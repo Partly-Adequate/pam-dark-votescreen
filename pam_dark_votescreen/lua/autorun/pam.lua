894fca621a4f81a4c3bb60dcf4ae330669cbec51
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-01-03 12:20:30 +0100

    Initial code

---
+++ pam_dark_votescreen/lua/autorun/pam.lua
@@ -0,0 +1,8 @@
+if SERVER then
+	-- add resources
+	resource.AddFile("materials/vgui/pam/img_missing.vmt")
+	resource.AddFile("materials/vgui/pam/ic_favorite.vmt")
+	resource.AddFile("materials/vgui/pam/ic_not_favorite.vmt")
+	resource.AddFile("materials/vgui/pam/ic_selected.vmt")
+	resource.AddFile("materials/vgui/pam/ic_not_selected.vmt")
+end
